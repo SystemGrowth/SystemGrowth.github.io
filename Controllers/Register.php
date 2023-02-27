e88d240d0f00f14f050c95887ca1b324fb8a2532
SystemGrowth <126357074+SystemGrowth@users.noreply.github.com>
2023-02-26 20:45:50 -0500

    v4

---
+++ Controllers/Register.php
@@ -0,0 +1,15 @@
+<?php 
+	class Register extends Controllers{
+		public function __construct()
+		{
+			parent::__construct();
+		}
+
+		public function register()
+		{
+			$data['page_name'] = "tienda_virtual";
+			$this->views->getView($this,"register",$data); 
+		}
+
+	}
+ ?>
