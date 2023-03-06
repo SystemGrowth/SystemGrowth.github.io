a3d8af6a0f99a12d2dfd373a6eb9bb8d4b2285e6
unknown <leydencitogames@gmail.com>
2023-03-05 19:06:59 -0500

    v5

--- Views/Template/footer_admin.php
+++ Views/Template/footer_admin.php
@@ -1,47 +1,81 @@
 
-<footer class="bg3 p-t-75 p-b-32">
-	<h1 align=center>ADADADADADAD</h1>
+<?php
+	$anio_actual = date("Y");
+	$nombre_autor = "Jeanpier Palomino - Dante Revilla";
+?>
+<footer style="text-align:center; position:fixed;left:0;bottom: 0;width: 100%;background-color: #222D32;padding: 3px;color:white;">
+  	<p>&copy; <?php echo $anio_actual; ?> <?php echo $nombre_autor; ?>. Todos los derechos reservados.</p>
 </footer>
-    <script>
+<script>
         const base_url = "<?= base_url(); ?>";
         const smony = "<?= SMONEY; ?>";
     </script>
-
 		<!-- Essential javascripts for application to work-->
     <script src="<?= media(); ?>/js/jquery-3.3.1.min.js"></script>
     <script src="<?= media(); ?>/js/popper.min.js"></script>
     <script src="<?= media(); ?>/bootstrap/js/bootstrap.min.js"></script>
     <script src="<?= media(); ?>/js/main.js"></script>
-		<script src="<?= media(); ?>/js/fontawesome.js"></script>
-
-
     <!-- The javascript plugin to display page loading on top-->
     <script src="<?= media(); ?>/js/plugins/pace.min.js"></script>
-
     <!-- Page specific javascripts-->
-		<script type="text/javascript" src="<?= media();?>/js/plugins/sweetalert.min.js"></script>
-    <script type="text/javascript" src="<?= media(); ?>/js/tinymce/tinymce.min.js"></script>
-
-		<!-- Data table plugin-->
-    <script type="text/javascript" src="<?= media(); ?>/js/plugins/jquery.dataTables.min.js"></script>
-    <script type="text/javascript" src="<?= media(); ?>/js/plugins/dataTables.bootstrap.min.js"></script>
-    <script type="text/javascript" src="<?= media();?>/js/plugins/bootstrap-select.min.js"></script>
-    <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/1.5.2/js/dataTables.buttons.min.js"></script>
-    <script type="text/javascript" language="javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-    <script type="text/javascript" language="javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
-    <script type="text/javascript" language="javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
-    <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.html5.min.js"></script>
-    <script src="https://code.highcharts.com/highcharts.js"></script>
-    <script src="https://code.highcharts.com/modules/exporting.js"></script>
-    <script src="https://code.highcharts.com/modules/export-data.js"></script>
-    <script src="<?= media();?>/js/datepicker/jquery-ui.min.js"></script>
-    <script type="text/javascript" src="<?= media();?>/js/functions_admin.js"></script>
-    <script src="<?= media(); ?>/js/<?= $data['page_functions_js']; ?>"></script>
-
-
-
-
+    <script type="text/javascript" src="<?= media(); ?>/js/plugins/chart.js"></script>
+    <script type="text/javascript">
+      var data = {
+      	labels: ["January", "February", "March", "April", "May"],
+      	datasets: [
+      		{
+      			label: "My First dataset",
+      			fillColor: "rgba(220,220,220,0.2)",
+      			strokeColor: "rgba(220,220,220,1)",
+      			pointColor: "rgba(220,220,220,1)",
+      			pointStrokeColor: "#fff",
+      			pointHighlightFill: "#fff",
+      			pointHighlightStroke: "rgba(220,220,220,1)",
+      			data: [65, 59, 80, 81, 56]
+      		},
+      		{
+      			label: "My Second dataset",
+      			fillColor: "rgba(151,187,205,0.2)",
+      			strokeColor: "rgba(151,187,205,1)",
+      			pointColor: "rgba(151,187,205,1)",
+      			pointStrokeColor: "#fff",
+      			pointHighlightFill: "#fff",
+      			pointHighlightStroke: "rgba(151,187,205,1)",
+      			data: [28, 48, 40, 19, 86]
+      		}
+      	]
+      };
+      var pdata = [
+      	{
+      		value: 300,
+      		color: "#46BFBD",
+      		highlight: "#5AD3D1",
+      		label: "Complete"
+      	},
+      	{
+      		value: 50,
+      		color:"#F7464A",
+      		highlight: "#FF5A5E",
+      		label: "In-Progress"
+      	}
+      ]
+      
+      var ctxl = $("#lineChartDemo").get(0).getContext("2d");
+      var lineChart = new Chart(ctxl).Line(data);
+      
+      var ctxp = $("#pieChartDemo").get(0).getContext("2d");
+      var pieChart = new Chart(ctxp).Pie(pdata);
+    </script>
+    <!-- Google analytics script-->
+    <script type="text/javascript">
+      if(document.location.hostname == 'pratikborsadiya.in') {
+      	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      	ga('create', 'UA-72504830-1', 'auto');
+      	ga('send', 'pageview');
+      }
+    </script>
   </body>
 </html>
-
-
